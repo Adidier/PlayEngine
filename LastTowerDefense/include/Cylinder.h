c4fe6a06761993cc3225ec3ccfaade2ed5bbd029
Cesar Himura <cesarhimura@gmail.com>
2022-10-03 19:17:47 -0500

    Correción de esfera y se agrega el cilindro con físicas

---
+++ LastTowerDefense/include/Cylinder.h
@@ -0,0 +1,17 @@
+#include "Base/GameObject.h"
+#include "Graphic/Model.h"
+#include "Physics/RigidBody.h"
+
+class Cylinder : public GameObject
+{
+	private:
+		RigidBody* rigidbody;
+		Transform transform;
+		Graphic::Model* model;
+	public:
+		Cylinder();
+		virtual ~Cylinder();
+		void Draw() override;
+		void Update(unsigned int delta) override;
+		void OnTriggerEnter(void* ptr) override;
+};
