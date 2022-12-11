2af4032edf118774720407b6aa46061ea484d583
Adidier <adidier.perez@gmail.com>
2022-12-11 02:38:14 -0600

    added collision box

---
+++ PlayEngine/Graphic/CameraFPS.cpp
@@ -0,0 +1,105 @@
+#include "Graphic/CameraFPS.h"
+#include "Base/ResourceManager.h"
+CameraFPS::CameraFPS() 
+{
+}
+
+CameraFPS::CameraFPS(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed)
+{
+	firstMove = true;
+	nextPosition = position = startPosition;
+	worldUp = startUp;
+	yaw = startYaw;
+	pitch = startPitch;
+	
+	front = glm::vec3(0.0f, 0.0f, 1.0f);
+
+	moveSpeed = startMoveSpeed;
+	turnSpeed = startTurnSpeed;
+
+	update();
+}
+
+void CameraFPS::keyControl(std::map<int, bool> keys, GLfloat deltaTime)
+{
+	GLfloat velocity = moveSpeed * deltaTime;
+
+	if (keys[GLFW_KEY_W])
+	{
+		nextPosition += front * velocity;
+	}
+
+	if (keys[GLFW_KEY_S])
+	{
+		nextPosition -= front * velocity;
+	}
+
+	if (keys[GLFW_KEY_A])
+	{
+		nextPosition -= right * velocity;
+	}
+
+	if (keys[GLFW_KEY_D])
+	{
+		nextPosition += right * velocity;
+	}
+
+	if (keys[GLFW_KEY_D])
+	{
+		nextPosition += right * velocity;
+	}
+}
+
+glm::vec3 CameraFPS::getCameraPosition()
+{
+	return nextPosition;
+}
+
+glm::vec3 CameraFPS::getFront()
+{
+	return front;
+}
+
+void CameraFPS::mouseControl(GLfloat xChange, GLfloat yChange)
+{
+	if (firstMove) {
+		prevXPos = xChange;
+		prevYPos = yChange;
+		firstMove = false;
+	}
+
+	float xoffset = xChange - prevXPos;
+	float yoffset = yChange - prevYPos;
+
+	prevXPos = xChange;
+	prevYPos = yChange;
+
+	xoffset *= turnSpeed;
+	yoffset *= turnSpeed;
+
+	yaw += xoffset;
+	pitch += yoffset;
+	update();
+}
+
+glm::mat4 CameraFPS::calculateViewMatrix()
+{
+	position = nextPosition;
+	return glm::lookAt(position, position + front, up);
+}
+
+void CameraFPS::update()
+{
+	front.x = cos(glm::radians(-yaw));
+	front.y = sin(glm::radians(-pitch));
+	front.z = sin(glm::radians(-yaw));
+	front = glm::normalize(front);
+	
+	right = glm::normalize(glm::cross(front, worldUp));
+	up = glm::normalize(glm::cross(right, front));
+}
+
+CameraFPS::~CameraFPS()
+{
+}
+	
